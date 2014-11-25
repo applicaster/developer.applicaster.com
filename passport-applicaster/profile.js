bb09b323d121ecf8b4d427fac46a64bf1a706119
gavriguy <gavri.guy@gmail.com>
2014-11-25 23:27:58 +0200

    clenup

--- passport-applicaster/profile.js
+++ passport-applicaster/profile.js
@@ -11,11 +11,5 @@ exports.parse = function(json) {
   }
   var profile = {};
   profile.displayName = json.name;
-  // profile.id = String(json.identity.id);
-  // profile.displayName = json.identity.first_name + ' ' + json.identity.last_name;
-  // profile.name = { familyName: json.identity.last_name,
-  //                  givenName: json.identity.first_name };
-  // profile.emails = [{ value: json.identity.email_address }];
-
   return profile;
 };
