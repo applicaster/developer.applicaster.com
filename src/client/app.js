b049db039980bc1ebaa9a04e4b33f04d39965aad
Gavri (Gabriel) Guy <gavri.guy@gmail.com>
2015-08-19 09:21:28 +0300

    add welcome homepage

--- src/client/app.js
+++ src/client/app.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Router from 'react-router';
+import { Styles } from 'material-ui';
 import { Route, RouteHandler, Redirect } from 'react-router';
 import PageHandler from './components/Page';
 import MainHandler from './components/Main';
 import ProductListHandler from './components/ProductList';
+import Home from './components/Home';
 import { createRedux } from 'redux';
 import { Provider } from 'redux/react';
 import * as stores from './stores';
 
 import './common/stylesheets/base.scss';
-
+const ThemeManager = new Styles.ThemeManager();
 const redux = createRedux(stores);
-
 const App = React.createClass({
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object,
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme(),
+    };
+  },
 
   render() {
     return (
@@ -28,10 +39,11 @@ const App = React.createClass({
 const routes = (
   <Route name="app" path="/" handler={App}>
     <Route name="main" path="/released" handler={MainHandler}>
-      <Route name="page" path="/:type/:page" handler={PageHandler}/>
+      <Route name="home" path="/home" handler={Home}/>
       <Route name="productsList" path="/products-list" handler={ProductListHandler}/>
+      <Route name="page" path="/:type/:page" handler={PageHandler}/>
     </Route>
-    <Redirect from="*" to="productsList"/>
+    <Redirect from="*" to="home"/>
   </Route>
 );
 
---
+++ src/client/components/SectionHeader/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default class SectionHeader extends Component {
+
+  static propTypes = {
+    title: PropTypes.string,
+  };
+
+  render() {
+
+    const sectionHeaderStyle = {
+      fontWeight: '300',
+      fontSize: '1.5em',
+      color: '#666',
+      lineHeight: '3em',
+      borderBottom: 'solid 1px #D3D3D3',
+      marginBottom: '1em',
+    };
+
+    return (
+        <h2 style={sectionHeaderStyle} >{ this.props.title }</h2>
+    );
+  }
+}
+
