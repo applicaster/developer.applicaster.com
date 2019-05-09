9822b96d034f1f89434d58d586ab4dfd76320967
liviur <livius16@gmail.com>
2019-05-09 09:58:58 -0400

    fix(app.js): redirect http to https

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/_book'));
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
+app.use(express.static(__dirname + '/_book'));
+
 app.listen(process.env.PORT || 3000);
